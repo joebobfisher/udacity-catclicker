f2077e7032b821c466130b262391a99df9f320a9
Joe Fisher <joe.fisher@distek.com>
2018-02-10 20:26:39 -0600

    Added second kitty.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
-var counter = 0;
-var $counter_space = $('#counterspace');
+var k1clicks = 0;
+var k2clicks = 0;
+var $k1_counter_space = $('#k1counter');
+var $k2_counter_space = $('#k2counter');
 
-$('#kitty').click(function(e) {
-    counter++;
-    $counter_space.text('Counter: ' + counter);
+var k1_name = "Kitty #1";
+var k2_name = "Kitty #2";
+var $k1_name_space = $('#k1name');
+var $k2_name_space = $('#k2name');
+
+$k1_name_space.text(k1_name);
+$k1_counter_space.text("");
+$k2_name_space.text(k2_name);
+$k2_counter_space.text("");
+
+$('#kitty1').click(function(e) {
+    k1clicks++;
+    $k1_counter_space.text('Counter: ' + k1clicks);
+});
+
+$('#kitty2').click(function(e) {
+    k2clicks++;
+    $k2_counter_space.text('Counter: ' + k2clicks);
 });
