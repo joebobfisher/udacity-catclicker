51e9ac4d1ba253ced05a2204450dd798736492be
Joe Fisher <joe.fisher@distek.com>
2018-02-10 22:54:56 -0600

    Multiple kitties -- now much more easily expandable.

--- js/script.js
+++ js/script.js
@@ -1,24 +1,32 @@
-var k1clicks = 0;
-var k2clicks = 0;
-var $k1_counter_space = $('#k1counter');
-var $k2_counter_space = $('#k2counter');
+var cats = [{name:"Kitty #1", counter:0, image:"img/kitty1.gif"},
+            {name:"Kitty #2", counter:0, image:"img/kitty2.gif"},
+            {name:"Kitty #3", counter:0, image:"img/kitty3.gif"},
+            {name:"Kitty #4", counter:0, image:"img/kitty4.gif"},
+            {name:"Kitty #5", counter:0, image:"img/kitty5.gif"}];
 
-var k1_name = "Kitty #1";
-var k2_name = "Kitty #2";
-var $k1_name_space = $('#k1name');
-var $k2_name_space = $('#k2name');
+var $k_list = $('#kitty-list');
 
-$k1_name_space.text(k1_name);
-$k1_counter_space.text("");
-$k2_name_space.text(k2_name);
-$k2_counter_space.text("");
+var $k_name = $('#kitty-name');
+var $k_image = $('#kitty-img');
+var $k_counter = $('#kitty-counter');
 
-$('#kitty1').click(function(e) {
-    k1clicks++;
-    $k1_counter_space.text('Counter: ' + k1clicks);
-});
+var selected;
 
-$('#kitty2').click(function(e) {
-    k2clicks++;
-    $k2_counter_space.text('Counter: ' + k2clicks);
+function showKitty(kitty_num) {
+    selected = kitty_num;
+    $k_name.text(cats[kitty_num].name);
+    $k_image.html("<img src='" + cats[kitty_num].image + "'>");
+    $k_counter.text("Counter: " + cats[kitty_num].counter);
+}
+
+for (var i=0; i < cats.length; i++) {
+    $k_list.append("<li><a href='#' onclick=showKitty("+ i +")>"+ cats[i].name +"</a></li>");
+}
+
+$k_name.text("Select a kitty to the left!");
+$k_counter.text("");
+
+$k_image.click(function(e) {
+    cats[selected].counter++;
+    $k_counter.text('Counter: ' + cats[selected].counter);
 });
