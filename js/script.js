3c9e6782e72950bc000e5285cb2b345b6f2c03a8
Joe Fisher <joe.fisher@distek.com>
2018-02-11 20:03:19 -0600

    Changed to Model-View-Octopus scheme.

--- js/script.js
+++ js/script.js
@@ -1,32 +1,83 @@
-var cats = [{name:"Kitty #1", counter:0, image:"img/kitty1.gif"},
-            {name:"Kitty #2", counter:0, image:"img/kitty2.gif"},
-            {name:"Kitty #3", counter:0, image:"img/kitty3.gif"},
-            {name:"Kitty #4", counter:0, image:"img/kitty4.gif"},
-            {name:"Kitty #5", counter:0, image:"img/kitty5.gif"}];
+// Model //
+var model = { current_kitty: null,
+              cats: [ {name:"Kitty #1", counter:0, image:"img/kitty1.gif"},
+                      {name:"Kitty #2", counter:0, image:"img/kitty2.gif"},
+                      {name:"Kitty #3", counter:0, image:"img/kitty3.gif"},
+                      {name:"Kitty #4", counter:0, image:"img/kitty4.gif"},
+                      {name:"Kitty #5", counter:0, image:"img/kitty5.gif"} ] };
 
-var $k_list = $('#kitty-list');
+// Views //
+var viewCat = {
+    init: function() {
+        this.countDom = document.getElementById('kitty-counter');
+        this.nameDom = document.getElementById('kitty-name');
+        this.imgDom = document.getElementById('kitty-img');
+        this.imgDom.addEventListener('click', function() {
+            octopus.increment();
+        });
+        this.render();
+    },
 
-var $k_name = $('#kitty-name');
-var $k_image = $('#kitty-img');
-var $k_counter = $('#kitty-counter');
+    // this guy does the work of the previous "showKitty()"
+    render: function() {
+        var current = octopus.getCurrentKitty();
+        this.countDom.textContent = current.counter;
+        this.nameDom.textContent = current.name;
+        this.imgDom.src = current.image;
+    },
 
-var selected;
+    // need this one so that the gif doesn't reset every time it is rendered...
+    renderCounter: function() {
+        var current = octopus.getCurrentKitty();
+        this.countDom.textContent = current.counter;
+    }
+};
 
-function showKitty(kitty_num) {
-    selected = kitty_num;
-    $k_name.text(cats[kitty_num].name);
-    $k_image.html("<img src='" + cats[kitty_num].image + "'>");
-    $k_counter.text("Counter: " + cats[kitty_num].counter);
-}
+var viewCatList = {
+    init: function() {
+        this.catlistJQueryObj = $('#kitty-list');
+        this.render();
+    },
 
-for (var i=0; i < cats.length; i++) {
-    $k_list.append("<li><a href='#' onclick=showKitty("+ i +")>"+ cats[i].name +"</a></li>");
-}
+    render: function() {
+        var cats = octopus.getCats();
+        for (var i = 0; i < cats.length; i++) {
 
-$k_name.text("Select a kitty to the left!");
-$k_counter.text("");
+            // this seems a little easier than addEventListener()
+            this.catlistJQueryObj.append('<li><a href="#" onclick=octopus.setCurrentAndRenderKitty('+ i +')>'+ cats[i].name +'</a></li>');
+        }
+    }
+};
 
-$k_image.click(function(e) {
-    cats[selected].counter++;
-    $k_counter.text('Counter: ' + cats[selected].counter);
-});
+// Octopus //
+var octopus = {
+    init: function() {
+        // init model
+        model.current_kitty = model.cats[0];
+
+        // init views
+        viewCatList.init();
+        viewCat.init();
+    },
+
+    getCats: function() {
+        return model.cats;
+    },
+
+    getCurrentKitty: function() {
+        return model.current_kitty;
+    },
+
+    setCurrentAndRenderKitty: function(kitty_num) {
+        model.current_kitty = model.cats[kitty_num];
+        viewCat.render();
+    },
+
+    increment: function() {
+        model.current_kitty.counter++;
+        viewCat.renderCounter();
+    }
+};
+
+// Do it.
+octopus.init();
