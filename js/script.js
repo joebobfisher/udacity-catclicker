f096ca8aca6bdbf0f633ce13b27c26106a40e9c3
Joe Fisher <joe.fisher@distek.com>
2018-02-11 21:29:34 -0600

    Added pro features.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 // Model //
-var model = { current_kitty: null,
-              cats: [ {name:"Kitty #1", counter:0, image:"img/kitty1.gif"},
-                      {name:"Kitty #2", counter:0, image:"img/kitty2.gif"},
-                      {name:"Kitty #3", counter:0, image:"img/kitty3.gif"},
-                      {name:"Kitty #4", counter:0, image:"img/kitty4.gif"},
-                      {name:"Kitty #5", counter:0, image:"img/kitty5.gif"} ] };
+var model = {
+    current_kitty: null,
+
+    cats: [ {name:"Kitty #1", counter:0, image:"img/kitty1.gif"},
+            {name:"Kitty #2", counter:0, image:"img/kitty2.gif"},
+            {name:"Kitty #3", counter:0, image:"img/kitty3.gif"},
+            {name:"Kitty #4", counter:0, image:"img/kitty4.gif"},
+            {name:"Kitty #5", counter:0, image:"img/kitty5.gif"} ]
+};
 
 // Views //
 var viewCat = {
@@ -49,6 +52,75 @@ var viewCatList = {
     }
 };
 
+var viewAdmin = {
+    init: function() {
+        this.adminMode = false;
+
+        this.buttonDom = document.getElementById('admin-button-area');
+        this.adminDom = document.getElementById('admin-area');
+        this.nameFieldDom = document.getElementById('name-field');
+        this.imgFieldDom = document.getElementById('img-url-field');
+        this.counterFieldDom = document.getElementById('counter-field');
+        this.okDom = document.getElementById('ok-button');
+        this.cancelDom = document.getElementById('cancel-button');
+
+        this.render();
+    },
+
+    render: function() {
+        var current;
+
+        if  (this.adminMode === true)
+        {
+            current = octopus.getCurrentKitty();
+            this.nameFieldDom.value = current.name;
+            this.imgFieldDom.value = current.image;
+            this.counterFieldDom.value = current.counter;
+
+            this.buttonDom.style.display = "none";
+            this.adminDom.style.display = "block";
+        }
+        else
+        {
+            this.adminDom.style.display = "none";
+            this.buttonDom.style.display = "block";
+        }
+    },
+
+    showButton: function() {
+        this.adminMode = false;
+        this.render();
+    },
+
+    showAdmin: function() {
+        this.adminMode = true;
+        this.render();
+    },
+
+    ok: function() {
+        if (this.adminMode === true)
+        {
+            var current = octopus.getCurrentKitty();
+            current.name = this.nameFieldDom.value;
+            current.image = this.imgFieldDom.value;
+            current.counter = this.counterFieldDom.value;
+            this.showButton();
+            octopus.renderKitty();
+        }
+    },
+
+    cancel: function() {
+        if (this.adminMode === true)
+        {
+            var current = octopus.getCurrentKitty();
+            this.nameFieldDom.value = current.name;
+            this.imgFieldDom.value = current.image;
+            this.counterFieldDom.value = current.counter;
+            this.showButton();
+        }
+    }
+};
+
 // Octopus //
 var octopus = {
     init: function() {
@@ -58,6 +130,7 @@ var octopus = {
         // init views
         viewCatList.init();
         viewCat.init();
+        viewAdmin.init();
     },
 
     getCats: function() {
@@ -76,6 +149,10 @@ var octopus = {
     increment: function() {
         model.current_kitty.counter++;
         viewCat.renderCounter();
+    },
+
+    renderKitty: function() {
+        viewCat.render();
     }
 };
 
